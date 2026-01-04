1b9f1a4250fa554e79e339d092d3094c35aacf91
Yusheng Yang <yangyusheng12323@outlook.com>
2026-01-04 16:41:25 +0800

    fix(rename_image_names): enhance logging setup and processing logic

--- scripts/rename_image_names.py
+++ scripts/rename_image_names.py
@@ -1,11 +1,31 @@
 # %% ------------------------------------ Import libraries ------------------------------------ #
 import sys
 from pathlib import Path
+from loguru import logger
 
 sys.path.append(str(Path(__file__).parent.parent / "src"))
 from utils import verificationMetadata, roundConfig
 from rename_functions import rename_images_per_round
 from table_organizer import TableConfig, process_all_rounds
+
+# %% ------------------------------------ Logger setup ------------------------------------ #
+logger.remove()
+logger.add(
+    sys.stdout,
+    format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | "
+           "<level>{level: <8}</level> | "
+           "<cyan>{module: <20}</cyan>:<cyan>{line: <4}</cyan> - | "
+           "<level>{message}</level>",
+    level="INFO"
+)
+
+logger.add(
+    "../logs/rename_image_names.log",
+    format="{time:YYYY-MM-DD HH:mm:ss} | {level: <8} | {module: <20}:{line: <4} - | {message}",
+    level="DEBUG",
+    mode="w"
+)
+
 # %% ------------------------------------ Main script ------------------------------------ #
 
 verification_meta = verificationMetadata()
@@ -13,11 +33,16 @@ verification_meta = verificationMetadata()
 default_round_config = roundConfig()
 
 all_rounds = list(default_round_config.raw_data_folder_path.iterdir())
+not_processed_name = [
+    "22th_round",
+]
 for round_path in all_rounds:
     if round_path.is_dir():
         round_name = round_path.name
         print(f"Processing round: {round_name}")
-
+        if round_name in not_processed_name:
+            # print(f"Skipping unrecognized round: {round_name}")
+            continue
         rename_images_per_round(
             round=round_name,
             verification_metadata=verification_meta,
